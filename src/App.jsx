c500da3960bda6a03e464bf7caa39fba1a3a5a9e
Weggins <Emmanuelmensah735@gmail.com>
2021-08-09 17:07:55 +0000

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieReviews from './Components/MovieReviews';
+
+function App() {
+  return (
+    <div>
+      <MovieReviews />
+    </div>
+  );
+}
+
+export default App;
